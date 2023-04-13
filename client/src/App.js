0b6f985f3acaef1f82c04f21e1732efdc9fc58ea
abdishukri <abdishukrimohamed105@gmail.com>
2023-04-13 20:17:05 +0300

    testing ci

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Abdishukri moahmed adan
         </p>
         <a
           className="App-link"
